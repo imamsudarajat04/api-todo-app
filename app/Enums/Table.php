1b23acc9f595b7ab373a44b6f6f6519f0763bc5f
imamsudarajat04 <imamsudarajat708@gmail.com>
2025-06-18 19:42:02 +0700

    first commit

---
+++ app/Enums/Table.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Enums;
+
+enum Table: string
+{
+    case USERS = 'users';
+    case PASSWORD_RESET_TOKENS = 'password_reset_tokens';
+    case SESSIONS = 'sessions';
+    case CACHE = 'cache';
+    case CACHE_LOCKS = 'cache_locks';
+    case JOBS = 'jobs';
+    case JOB_BATCHES = 'job_batches';
+    case FAILED_JOBS = 'failed_jobs';
+    case TODOS = 'todos';
+}
